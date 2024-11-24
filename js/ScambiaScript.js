4bb1bb80418861969cb3b7703d21d8f0ed11b67a
danieltonin <daniel.tonin@iisvittorioveneto.it>
2024-11-24 17:22:28 +0100

    v8

--- js/ScambiaScript.js
+++ js/ScambiaScript.js
@@ -1,27 +1,18 @@
-function scambia(){
-	
-	
-	
-	let img = document.getElementById("img").src;
-	
-	let img1 = document.getElementById("img1").src;
-	
-	let img2 = document.getElementById("img2").src;
-	
-	let img3 = document.getElementById("img3").src;
-	
-	
-	
-	
-	let vettimg[3] = {img, img1, img2, img3};
-	
-	let buffer = vettimg[3];
-	
-	document.getElementById("img3").src = vettimg[0];
-	
-	document.getElementById("img").src = vettimg[1];
-	
-	document.getElementById("img2").src = buffer;
-	
-	
+function scambia() {
+    let img = document.getElementById("img").src;
+    let img1 = document.getElementById("img1").src;
+    let img2 = document.getElementById("img2").src;
+    let img3 = document.getElementById("img3").src;
+
+    
+    let vettimg = [img, img1, img2, img3];
+
+    
+    let buffer = vettimg[0];
+
+    
+    document.getElementById("img").src = vettimg[1]; 
+    document.getElementById("img1").src = vettimg[2]; 
+    document.getElementById("img2").src = vettimg[3]; 
+    document.getElementById("img3").src = buffer;     
 }
